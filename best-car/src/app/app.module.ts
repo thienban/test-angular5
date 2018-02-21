1e7ace502e41179ffb4e31d06f0ced5a2b75c9d7
thienban <thienban91@hotmail.fr>
2018-02-21 20:34:42 +0100

    install datatable

--- best-car/src/app/app.module.ts
+++ best-car/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-
+import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
 
 
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    DataTablesModule,
     NgbModule.forRoot()
   ],
   providers: [],
